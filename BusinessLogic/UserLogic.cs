045ee42d21721a1f2e8f283812ffd20fcf8d22f2
Federico De Alava <fd288858@fi365.ort.edu.uy>
2024-04-22 12:33:00 -0300

    CreateUser Refactor

--- BusinessLogic/UserLogic.cs
+++ BusinessLogic/UserLogic.cs
@@ -13,8 +13,29 @@ namespace BusinessLogic
             this._userRepository = userRepository;
         }
 
-        public User CreateUser(User user) { 
-            return _userRepository.CreateUser(user);
+        public User CreateUser(User user) {
+            try
+            {
+                if (user == null)
+                {
+                    throw new ArgumentNullException(nameof(user), "User can't be null");
+                }
+
+                if (string.IsNullOrWhiteSpace(user.Email) ||
+                    string.IsNullOrWhiteSpace(user.Name) ||
+                    string.IsNullOrWhiteSpace(user.Password) ||
+                    string.IsNullOrWhiteSpace(user.Role))
+                {
+                    throw new ArgumentException("Invalid data");
+                }
+
+                _userRepository.CreateUser(user);
+                return user;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Unable to create user", ex);
+            }
         }
     }
 }
