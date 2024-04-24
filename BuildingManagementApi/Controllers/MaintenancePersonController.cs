18edf4e3822517d37c61ea7a03e291f0333c492e
seba17Jordan <SJ285318@fi365.ort.edu.uy>
2024-04-24 11:55:59 -0300

    CreateMaintenancePersonCorrectTest Green

---
+++ BuildingManagementApi/Controllers/MaintenancePersonController.cs
@@ -0,0 +1,31 @@
+﻿using LogicInterface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelsApi.In;
+using ModelsApi.Out;
+
+namespace BuildingManagementApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MaintenancePersonController : ControllerBase
+    {
+        private readonly IMaintenancePersonLogic _maintenancePersonLogic;
+
+        public MaintenancePersonController(IMaintenancePersonLogic maintenancePersonLogic)
+        {
+            _maintenancePersonLogic = maintenancePersonLogic;
+        }
+
+        [HttpPost]
+        public IActionResult CreateMaintenancePerson([FromBody] MaintenancePersonRequest maintenancePersonRequest)
+        {
+            var maintenancePerson = maintenancePersonRequest.ToEntity();
+            var createdMaintenancePerson = _maintenancePersonLogic.CreateMaintenancePerson(maintenancePerson);
+            var outputResult = new MaintenancePersonResponse(createdMaintenancePerson);
+
+            return CreatedAtAction(nameof(CreateMaintenancePerson), new { id = outputResult.Id }, outputResult);
+        }
+
+    }
+}
---
+++ BuildingManagementApiTest/MaintenanceControllerTest.cs
@@ -0,0 +1,51 @@
+﻿using BuildingManagementApi.Controllers;
+using Domain;
+using LogicInterface;
+using Microsoft.AspNetCore.Mvc;
+using ModelsApi.In;
+using ModelsApi.Out;
+using Moq;
+
+namespace BuildingManagementApiTest;
+
+[TestClass]
+public class MaintenanceControllerTest
+{
+    [TestMethod]
+    public void CreateMaintenancePersonCorrectTest()
+    {
+        //Arrange
+        var maintenancePersonToCreate = new MaintenancePersonRequest
+        {
+            Name = "John",
+            LastName = "Doe",
+            Email = "john.doe@example.com",
+            Password = "strongPassword123"
+        };
+
+        var expectedMaintenancePerson = new User
+        {
+            Id = Guid.NewGuid(),
+            Name = "John",
+            LastName = "Doe",
+            Email = "john.doe@example.com",
+            Password = "hashedPassword"
+        };
+
+        var expectedMaintenancePersonResponse = new MaintenancePersonResponse(expectedMaintenancePerson);
+
+        var maintenancePersonLogicMock = new Mock<IMaintenancePersonLogic>();
+        maintenancePersonLogicMock.Setup(x => x.CreateMaintenancePerson(It.IsAny<User>())).Returns(expectedMaintenancePerson);
+
+        var maintenancePersonController = new MaintenancePersonController(maintenancePersonLogicMock.Object);
+
+        // Act
+        var result = maintenancePersonController.CreateMaintenancePerson(maintenancePersonToCreate) as CreatedAtActionResult;
+
+        // Assert
+        var createdMaintenancePerson = result.Value as MaintenancePersonResponse;
+        Assert.IsNotNull(createdMaintenancePerson);
+        Assert.AreEqual(expectedMaintenancePersonResponse, createdMaintenancePerson);
+    }
+
+}
