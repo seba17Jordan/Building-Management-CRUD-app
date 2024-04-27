8610194131f50bf7f104b3ad2f651d751afa727c
seba17Jordan <SJ285318@fi365.ort.edu.uy>
2024-04-27 19:57:11 -0300

    CreateBuildingNullShouldThrowExceptionTestLogic

--- BusinessLogicTest/BuildingLogicTest.cs
+++ BusinessLogicTest/BuildingLogicTest.cs
@@ -36,14 +36,63 @@ namespace BusinessLogicTest
             Mock<IBuildingRepository> buildingRepo = new Mock<IBuildingRepository>(MockBehavior.Strict);
             buildingRepo.Setup(l => l.CreateBuilding(It.IsAny<Building>())).Returns(expectedBuilding);
 
-            var buildingLogic = new BuildingLogic(buildingRepo.Object);
+            BuildingLogic buildingLogic = new BuildingLogic(buildingRepo.Object);
 
             //Act
-            var finalResult = buildingLogic.CreateBuilding(expectedBuilding);
+            Building logicResult = buildingLogic.CreateBuilding(expectedBuilding);
 
             //Assert
             buildingRepo.VerifyAll();
-            Assert.AreEqual(finalResult, expectedBuilding);
+            Assert.AreEqual(logicResult, expectedBuilding);
+        }
+
+        [TestMethod]
+        public void CreateBuildingNullShouldThrowExceptionTestLogic()
+        {
+            // Arrange
+            Exception specificEx = null;
+            Mock<IBuildingRepository> buildingRepo = new Mock<IBuildingRepository>(MockBehavior.Strict);
+            try
+            {
+                Building expectedBuilding = new Building()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Building 2",
+                    Address = "Address 2",
+                    ConstructionCompany = "Construction Company 1",
+                    CommonExpenses = 200,
+                    Apartments = new List<Apartment>
+                    {
+                        new Apartment()
+                        {
+                            Floor = 2,
+                            Number = 202,
+                            Owner = new Owner { Name = "Jane", LastName = "Doe", Email = "jane.doe@example.com" },
+                            Rooms = 2,
+                            Bathrooms = 2,
+                            HasTerrace = true
+                        }
+                    }
+                };
+                
+                buildingRepo.Setup(repo => repo.CreateBuilding(It.IsAny<Building>())).Returns(expectedBuilding);
+                
+                BuildingLogic buildingLogic = new BuildingLogic(buildingRepo.Object);
+
+                // Act
+                Building logicResult = buildingLogic.CreateBuilding(expectedBuilding);
+
+            }
+            catch (ArgumentException e)
+            {
+                specificEx = e;
+            }
+
+            // Assert
+            buildingRepo.VerifyAll();
+            Assert.IsNotNull(specificEx);
+            Assert.IsInstanceOfType(specificEx, typeof(ArgumentException));   //Crear exception especifica
+            Assert.AreEqual("Error", specificEx.Message);
         }
     }
 }
