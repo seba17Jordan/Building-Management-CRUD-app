0769d0186d4490618962b5baed3738cac89a39bb
Federico De Alava <fd288858@fi365.ort.edu.uy>
2024-04-27 18:48:49 -0300

    CreateCategory_ShouldReturnCreatedCategoryTest Green

---
+++ BusinessLogicTest/CategoryLogicTest.cs
@@ -0,0 +1,35 @@
+﻿using LogicInterface;
+using Domain;
+using Moq;
+using BusinessLogic;
+using IDataAccess;
+namespace BusinessLogicTest
+{
+    [TestClass]
+    public class CategoryLogicTest
+    {
+        [TestMethod]
+        public void CreateCategory_ShouldReturnCreatedCategoryTest()
+        {
+            // Arrange
+            var expectedCategory = new Category
+            {
+                Id = Guid.NewGuid(),
+                Name = "Category"
+            };
+
+            Mock<ICategoryRepository> repo = new Mock<ICategoryRepository>(MockBehavior.Strict);
+            repo.Setup(l => l.CreateCategory(It.IsAny<Category>())).Returns(expectedCategory);
+            repo.Setup(repo => repo.CategoryExists(It.IsAny<string>())).Returns(false);
+
+            var categoryLogic = new CategoryLogic(repo.Object);
+
+            // Act
+            var result = categoryLogic.CreateCategory(expectedCategory);
+
+            // Assert
+            repo.VerifyAll();
+            Assert.AreEqual(expectedCategory, result);
+        }
+    }
+}
