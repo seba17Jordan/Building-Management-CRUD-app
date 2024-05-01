57a258e3a3d855459e8d16807f00eaf4e11c7f5e
seba17Jordan <SJ285318@fi365.ort.edu.uy>
2024-05-01 03:41:26 -0300

    add: migration16

---
+++ DataAccess/Migrations/20240501063931_migration16.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class migration16 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "ServiceRequests",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ManagerId",
+                table: "ServiceRequests",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "ServiceRequests",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalCost",
+                table: "ServiceRequests",
+                type: "decimal(18,2)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "ServiceRequests");
+
+            migrationBuilder.DropColumn(
+                name: "ManagerId",
+                table: "ServiceRequests");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "ServiceRequests");
+
+            migrationBuilder.DropColumn(
+                name: "TotalCost",
+                table: "ServiceRequests");
+        }
+    }
+}
