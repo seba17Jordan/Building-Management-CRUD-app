f76a2ef38cb3e97b68d94a7f59378ba4e16cc652
seba17Jordan <SJ285318@fi365.ort.edu.uy>
2024-05-19 15:54:26 -0300

    GetConstructionCompanyByName_ShouldReturnConstructionCompanyDataAccessTest Green

---
+++ DataAccess/ConstructionCompanyRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain;
+using IDataAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess
+{
+    public class ConstructionCompanyRepository : IConstructionCompanyRepository
+    {
+        private readonly DbContext _context;
+        public ConstructionCompanyRepository(DbContext context)
+        {
+            _context = context;
+        }
+        public ConstructionCompany GetConstructionCompanyByName(string name)
+        {
+            return _context.Set<ConstructionCompany>().FirstOrDefault(x => x.Name == name);
+        }
+    }
+}
---
+++ DataAccessTest/ConstructionCompanyRepositoryTest.cs
@@ -0,0 +1,49 @@
+﻿using DataAccess;
+using Domain;
+using Domain.@enum;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+
+namespace DataAccessTest
+{
+    [TestClass]
+    public class ConstructionCompanyRepositoryTest
+    {
+        [TestMethod]
+        public void GetConstructionCompanyByName_ShouldReturnConstructionCompanyDataAccessTest()
+        {
+            // Arrange
+            var database = Guid.NewGuid().ToString();
+            var context = CreateDbContext(database);
+            ConstructionCompany constructionCompany = new ConstructionCompany()
+            {
+                Name = "Test",
+                ConstructionCompanyAdmin = new User()
+                {
+                    Email = "lkand@gmail.com",
+                    Password = "1234",
+                    Role = Roles.ConstructionCompanyAdmin,
+                    Name = "Lukas",
+                    LastName = "Kand"
+                }
+            };
+
+            context.Add(constructionCompany);
+            context.SaveChanges();
+            ConstructionCompanyRepository repository = new ConstructionCompanyRepository(context);
+
+            // Act
+            ConstructionCompany repoResult = repository.GetConstructionCompanyByName("Test");
+
+            // Assert
+            Assert.AreEqual(constructionCompany, repoResult);
+        }
+
+
+        private DbContext CreateDbContext(string database)
+        {
+            var options = new DbContextOptionsBuilder<Context>().UseInMemoryDatabase(database).Options;
+            return new Context(options);
+        }
+    }
+}
