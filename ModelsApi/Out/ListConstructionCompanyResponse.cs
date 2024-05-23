b2b2ad5e284c78223910bc797a66f0aadd0d80f8
Federico De Alava <fd288858@fi365.ort.edu.uy>
2024-05-22 21:28:06 -0300

    Fix: Error branch

---
+++ ModelsApi/Out/ListConstructionCompanyResponse.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelsApi.Out
+{
+    public class ListConstructionCompanyResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public bool HasManager { get; set; }
+        public string ManagerName { get; set; }
+
+        public ListConstructionCompanyResponse(Guid id, string name, string address, bool hasManager, string managerName)
+        {
+            Id = id;
+            Name = name;
+            Address = address;
+            HasManager = hasManager;
+            ManagerName = managerName;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is ListConstructionCompanyResponse response &&
+                   Id.Equals(response.Id) &&
+                   Name == response.Name &&
+                   Address == response.Address &&
+                   HasManager == response.HasManager &&
+                   ManagerName == response.ManagerName;
+        }
+    }
+
+}
