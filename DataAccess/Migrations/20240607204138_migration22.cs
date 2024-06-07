e41a12fd9ca739083350167c766bbcf304c724ba
seba17Jordan <SJ285318@fi365.ort.edu.uy>
2024-06-07 17:43:30 -0300

    add: migration 22

---
+++ DataAccess/Migrations/20240607204138_migration22.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class migration22 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CategoryName",
+                table: "ServiceRequests");
+
+            migrationBuilder.RenameColumn(
+                name: "Category",
+                table: "ServiceRequests",
+                newName: "CategoryId");
+
+            migrationBuilder.RenameColumn(
+                name: "Apartment",
+                table: "ServiceRequests",
+                newName: "ApartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ServiceRequests_ApartmentId",
+                table: "ServiceRequests",
+                column: "ApartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ServiceRequests_BuildingId",
+                table: "ServiceRequests",
+                column: "BuildingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ServiceRequests_CategoryId",
+                table: "ServiceRequests",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ServiceRequests_Apartments_ApartmentId",
+                table: "ServiceRequests",
+                column: "ApartmentId",
+                principalTable: "Apartments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ServiceRequests_Buildings_BuildingId",
+                table: "ServiceRequests",
+                column: "BuildingId",
+                principalTable: "Buildings",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ServiceRequests_Categories_CategoryId",
+                table: "ServiceRequests",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ServiceRequests_Apartments_ApartmentId",
+                table: "ServiceRequests");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ServiceRequests_Buildings_BuildingId",
+                table: "ServiceRequests");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ServiceRequests_Categories_CategoryId",
+                table: "ServiceRequests");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ServiceRequests_ApartmentId",
+                table: "ServiceRequests");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ServiceRequests_BuildingId",
+                table: "ServiceRequests");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ServiceRequests_CategoryId",
+                table: "ServiceRequests");
+
+            migrationBuilder.RenameColumn(
+                name: "CategoryId",
+                table: "ServiceRequests",
+                newName: "Category");
+
+            migrationBuilder.RenameColumn(
+                name: "ApartmentId",
+                table: "ServiceRequests",
+                newName: "Apartment");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CategoryName",
+                table: "ServiceRequests",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
